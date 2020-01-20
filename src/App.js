450be7fafd2c781ec2b9af68b4f14aefcf6c8f9e
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-01-20 15:43:32 +0200

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 import LayoutsList from './containers/LayoutsList';
 import LayoutEditor from './containers/LayoutEditor';
@@ -8,9 +13,17 @@ import './App.css';
 function App() {
   return (
     <>
-      <header position="absolute">
-        Layout configurator
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">
+            Layout Editor
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
       <Router>
         <Switch>
           <Route exact path="/">
