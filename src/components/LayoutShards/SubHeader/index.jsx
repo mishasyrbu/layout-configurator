3b5ec9b4528e9d4d69a2ac900a59a5209e92a98b
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-01-20 23:10:31 +0200

    add one more shard SubHeader

---
+++ src/components/LayoutShards/SubHeader/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.scss';
+
+export function SubHeader(props) {
+    return (
+        <div className="sc-subheader" data-componentid="SubHeader" {...props}>SubHeader</div>
+    );
+}
--- src/components/LayoutShards/index.js
+++ src/components/LayoutShards/index.js
@@ -1,9 +1,11 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
 import { Article } from './Article';
+import { SubHeader } from './SubHeader';
 
 export default {
     Header,
-    Footer,
+    SubHeader,
     Article,
+    Footer,
 };
