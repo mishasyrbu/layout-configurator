556e0b2466be6a339a14a045f20cf883ed011d4f
Misha Syrbu <mihaisyrbu91@gmail.com>
2020-01-20 20:01:42 +0200

    styling layouts list

--- src/containers/LayoutsList/index.jsx
+++ src/containers/LayoutsList/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import AddSharp from '@material-ui/icons/AddSharp';
+
+import './styles.scss';
 
 class LayoutsList extends React.Component {
     render() {
         return (
-            <div>Layouts List</div>
+            <div className="layouts-list">
+                <div className="title">{"Layouts Screen"}</div>
+                <div className="content">
+                    <div className="layout">
+                        <AddSharp className="add-icon" />
+                    </div>
+                </div>
+            </div>
         );
     }
 }
